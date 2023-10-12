e0b26a9c8a81b1fc23b6249ef7f5ad796e7a8801
Mohamed Aleya <mohamed.aleya@outlook.com>
2023-10-12 16:08:46 +0100

    Add dynamic layout to switch between teacher and guest modes

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Layout, Compass } from "lucide-react";
+import { Layout, Compass, List, BarChart } from "lucide-react";
 import { SidebarItem } from "./sidebar-item";
+
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   {
@@ -11,8 +13,25 @@ const guestRoutes = [
   },
   { icon: Compass, label: "Browse", href: "/search" },
 ];
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
 export const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.includes("/teacher");
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
   return (
     <div className="flex flex-col w-full">
