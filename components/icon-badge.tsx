0c91bc5385613a321bb1134b4dba47616bb1133c
Mohamed Aleya <mohamed.aleya@outlook.com>
2023-10-15 22:30:29 +0100

    Course setup part 1

---
+++ components/icon-badge.tsx
@@ -0,0 +1,25 @@
+import { LucideIcon } from "lucide-react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const backgroundVariant = cva("rounded-full flex items-center justify-center", {
+  variants: {
+    variant: {
+      default: "bg-sky-100",
+      success: "bg-emerald-100",
+    },
+    iconVariant: {
+      default: "text-sky-700",
+      success: "text-emerald-700",
+    },
+    size: {
+      default: "p-2",
+      sm: "p-1",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
